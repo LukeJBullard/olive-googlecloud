39ac954e36e30f7a746c26722437f36910c236a0
Luke Bullard <lukeb@agnesanddora.com>
2018-06-25 16:47:46 -0600

    handle multiple bigquery connections at the same time support for projectID being specified for the bigqueryclient

--- googlecloud.mod.php
+++ googlecloud.mod.php
@@ -26,26 +26,37 @@
         /**
          * Retrieves the BigQueryClient object. Creates the object if it has not been created already.
          * 
-         * @param String $a_keyFilePath The path to the json key file containing the service account credentials, or an empty string to use the environment variable value.
+         * @param String $a_projectID The project ID from the Google Developer's Console, or an empty string to omit.
+         * @param String $a_keyFilePath The path to the json key file containing the service account credentials,
+         *                  or an empty string to use the environment variable value.
          * @return BigQueryClient
          */
-        public function getBigQueryClient($a_keyFilePath = "keyfile.json")
+        public function getBigQueryClient($a_projectID = "", $a_keyFilePath = __DIR__ . "/keyfile.json")
         {
-            if (isset($this->m_clients['bigquery']))
+            $hash = "bigquery_" . md5(strtolower($a_projectID . $a_keyFilePath));
+
+            if (isset($this->m_clients[$hash]))
             {
-                return $this->m_clients['bigquery'];
+                return $this->m_clients[$hash];
             }
 
+            $bigQueryArgs = array();
+
+            //if the keyfile path was specified, process it
             if ($a_keyFilePath != "")
             {
-                $this->m_clients['bigquery'] = new BigQueryClient([
-                    'keyFilePath' => $a_keyFilePath
-                ]);
-            } else {
-                $this->m_clients['bigquery'] = new BigQueryClient();
+                $bigQueryArgs['keyFilePath'] = $a_keyFilePath;
             }
 
-            return $this->m_clients['bigquery'];
+            //if the projectID was set, process it
+            if ($a_projectID != "")
+            {
+                $bigQueryArgs['projectId'] = $a_projectID;
+            }
+
+            $this->m_clients[$hash] = new BigQueryClient($bigQueryArgs);
+
+            return $this->m_clients[$hash];
         }
     }
 ?>
